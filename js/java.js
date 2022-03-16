f63f4a7c7eb7b0e69f2f5c8c1f2d2797617e5811
patrikbenjaminn <patrik.kemppainen@gmail.com>
2022-03-16 19:14:03 +0200

    js funktio ajasta tehty

--- js/java.js
+++ js/java.js
@@ -1,5 +1,20 @@
-alert("Tervetuloa testisivulle");
+
+
 
 function button(){
+    let joku = document.getElementById("teksti").value;
+
+    let today = new Date()
+    let curHr = today.getHours()
+
+        if (curHr < 12) {
+  document.getElementById("vastaus").innerHTML = ('Hyvää huomenta ') + joku;
+    } else if (curHr < 18) {
+        document.getElementById("vastaus").innerHTML = ('Hyvää iltapäivää ') + joku;
+    } else {
+        document.getElementById("vastaus").innerHTML = ('Hyvää iltaa ') + joku;
+}
     
+    
+
 }
