03889c37eea910e4ceb8b5fa2613cda9b32b2225
patrikbenjaminn <patrik.kemppainen@gmail.com>
2022-04-10 17:48:36 +0300

    Tehty loput tehtävästä

--- DOM/code.js
+++ DOM/code.js
@@ -18,3 +18,14 @@ let bodyDes = document.querySelectorAll("body *");
 for(elem of bodyDes){
     console.log(elem.nodeName);
 }
+
+//e
+let articleDesc = document.querySelectorAll("article p");
+for (elem of articleDesc){
+    console.log(elem.textContent);
+}
+
+//f
+for(elem of elements){
+    elem.textContent = "John Doe";
+}
