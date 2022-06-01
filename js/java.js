31ccc7b3f7b4ffca7e37b64fb7f068474e42c2ac
patrikbenjaminn <patrik.kemppainen@gmail.com>
2022-06-01 17:41:13 +0300

    Tehty event background change

--- js/java.js
+++ js/java.js
@@ -9,3 +9,27 @@ $(".icon").attr('src', ikoni);
 $(".saa").append(saa);
 $(".Lämpötila").append(lampö);
 })
+
+var button = document.querySelector("#vihreä");
+
+function doClick(){
+    
+    document.body.style.backgroundImage = "url('../Kuvat/cool-background4.png')";
+    document.querySelector("#vihreä").remove();
+}
+
+
+button.addEventListener("click", doClick);
+
+var mail = document.querySelector("#mail");
+
+mail.addEventListener("click", maili );
+
+function maili() {
+    let p = document.createElement("p");
+    let node = document.createTextNode("Kiitos yhteydenotosta");
+    p.appendChild(node);
+    let element = document.querySelector(".lead")
+    element.appendChild(p);
+    
+}
